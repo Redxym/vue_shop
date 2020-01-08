57a22e25e531370767d84f56fb74524bc65f5fab
Redxym <2679521390@qq.com>
2020-01-08 15:23:17 +0800

    '完成商品功能’

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 import './assets/fonts/iconfont.css' // 导入字体图标
 import ZkTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor/src'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // 配置请求的根路径
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -17,8 +22,19 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI, { size: 'small', locale })
+Vue.use(VueQuillEditor)
 // Vue.use(ZkTable)
 Vue.component('tree-table', ZkTable)
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   store,
